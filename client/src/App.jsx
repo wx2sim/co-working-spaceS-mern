af527841e61d9520af927b9ce7b9bf9ca897977f
Wx2sim <wx2sim@gmail.com>
2026-04-02 14:34:45 +0100

    Create Header Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp' 
 import About from './pages/About'
+import Header from './components/Header'
 
 export default function App() {
   return (
    <BrowserRouter>  
+    <Header />
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/profile' element={<Profile />} />
