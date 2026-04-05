1573828624ddb48ecd5927f4b2898913f23cdd12
Wx2sim <wx2sim@gmail.com>
2026-04-05 10:47:51 +0100

    setting up redux-persist to store user data in the browser

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,11 @@
-import { FaSearch } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+
 
 function Header() {
+  const { currentUser } = useSelector(state => state.user);
   return (
     <header className='bg-slate-200 shadow-md'>
         <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
@@ -18,7 +22,15 @@ function Header() {
         <ul className='flex gap-4'>
            <Link to='/'> <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li> </Link>
            <Link to='/about'> <li className='hidden sm:inline text-slate-700 hover:underline'>About</li> </Link>
-           <Link to='/signin'> <li className=' text-slate-700 hover:underline'>Sign In</li> </Link>
+           {currentUser ? (
+            <img 
+            src={currentUser.avatar || 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'} 
+              className='rounded-full w-7 h-7 object-cover cursor-pointer' 
+              alt='profile'
+            />
+            ) : ( 
+              <Link to='/signin'>Sign In</Link> 
+            )}
         </ul>
         </div>
     </header>
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice.js'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import {persistReducer, persistStore, } from 'redux-persist';
+
+const storage = {
+  getItem: (key) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key, value) => Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const rootReducer = combineReducers({user: userReducer,});
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {user: userReducer},
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   })
-})
+});
+export const persistor = persistStore(store);
