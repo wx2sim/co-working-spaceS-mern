16266074b42d4da6616039f7ef4ca090d95560a4
Wx2sim <wx2sim@gmail.com>
2026-04-06 12:34:37 +0100

    create update user api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { user } from '../controllers/user.controller.js';
+import { updateUser, user } from '../controllers/user.controller.js';
+import {verifyUserToken} from '../utils/verifyUser.js'
 
 
 const router = express.Router();
 
 router.get('/user' , user);
+router.post('/update/:id' ,verifyUserToken , updateUser);
 
 export default router;
