946d7ee0956a338cf58eb6bbb4870ecb6d4b1295
Wx2sim <wx2sim@gmail.com>
2026-04-10 16:48:56 +0100

    creating the About page ui content

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import AnimatedPage from '../components/AnimatedPage'
 
-function About() {
+export default function About() {
   return (
     <AnimatedPage>
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Co-Spaces</h1>
+      <p className='mb-4 text-slate-700'>Co-Spaces is a leading Co-Working-Spaces agency that specializes in helping clients find and rent spaces in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the finding and renting process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our clients achieve their Profissional and confortability goals by providing the most suitable and elegant suggestions, personalized service, and a deep understanding of the local market. Whether you are looking to rent a space, ,make connections or benifit from our side services , we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents has a wealth of experience and knowledge in the remote working industry, and we are committed to providing the highest level of service to our clients. We believe that comfort or stablity  should be an present and provided for those who need, and we are dedicated to making that a reality for each and every one of our clients.</p>
+    </div>
     </AnimatedPage>
   )
 }
-
-export default About
