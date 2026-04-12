9b0cd146b97c727d326df2f5c3a88c23d8b76707
Wx2sim <wx2sim@gmail.com>
2026-04-12 11:22:44 +0100

    adding mailing funtion to send the email to the landlord

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -26,25 +26,25 @@ export default function Contact({ listing }) {
         <div className='flex flex-col gap-2'>
           <p>
             Contact <span className='font-semibold'>{landlord.username}</span>{' '}
-            for{' '}
-            <span className='font-semibold'>{listing.name.toLowerCase()}</span>
+            for{' '} <span className='font-semibold'>{listing.name.toLowerCase()}</span>
           </p>
           <textarea
             name='message'
             id='message'
-            rows='2'
+            rows='4'
             value={message}
             onChange={onChange}
             placeholder='Enter your message here...'
             className='w-full border p-3 rounded-lg'
           ></textarea>
-
-          <Link
-          to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
-          className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
+          <a
+            href={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+            target="_blank"
+            className='bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95'
           >
             Send Message          
-          </Link>
+          </a>
+          
         </div>
       )}
     </>
