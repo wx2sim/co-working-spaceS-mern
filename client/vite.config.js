aa7c6773f4c35e360f98bf1f4b8552290fb37372
Wx2sim <wx2sim@gmail.com>
2026-04-17 17:26:37 +0100

    vite config file update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,20 +4,13 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  server: {
-   proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-      }
-    }
-  },
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    
+
   ],
   optimizeDeps: {
-    include: ['antd'] 
+    include: ['antd']
   }
 })
